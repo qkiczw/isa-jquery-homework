24465b39c1543a1cfec961c22482baea202437aa
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-01-09 18:58:11 +0100

    Edit jquery.js, add some code.

--- jquery.js
+++ jquery.js
@@ -2,19 +2,30 @@ $(function() {
 
 const $body = $('body');
 
-
 $('<div></div>').addClass('container').prependTo($body);
 
 
 
-const createRows = (num) => {
-    for(let i = 0; i <= num; i++){
+const createRows = (rowsNum) => {
+    for ( let i = 0; i < rowsNum; i++ ) {
         $('<div></div>').addClass('row').appendTo('.container');
 
     }
 };
 
-createRows(2);
+
+const createCol = (rowNum, colNum) => {
+        for (let i = 0; i < colNum; i++) {
+            $('.row').eq(rowNum).append('<div></div>');
+
+        }
+    };
+
+createRows(3);
+createCol(0,3);
+createCol(1,2);
+createCol(2,3);
+
 
 
 });
