b3298018b55e31a8075ef7a6247f90bc35a5aa4f
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-01-10 20:44:07 +0100

    Add code in jquery.js.

--- jquery.js
+++ jquery.js
@@ -17,7 +17,7 @@ const createRows = (rowsNum) => {
 
 const createCol = (rowNum, colNum) => {
         for (let i = 0; i < colNum; i++) {
-            $('.row').eq(rowNum).append('<div></div>');
+            $('.row').eq(rowNum).append('<div><p>');
 
         }
     };
@@ -29,18 +29,40 @@ createCol(2,3);
 
 const $row = $('.row');
 const $colInRow = $('.row div');
+const $paragraph = $colInRow.find('p')
+const $colInFirstRow = $row.eq(0).find('div');
+const $colInSecondRow = $row.eq(1).find('div');
+const $colInThirdRow = $row.eq(2).find('div');
 
+$paragraph.addClass('description');
 
-
-
-
-const addColResAndSize = (scrRes, size) => {
+const addColResAndSize = ( scrRes, size ) => {
     return 'col-' + scrRes + '-' + size;
-}
+};
+
+$colInFirstRow.addClass( addColResAndSize('sm', 4) );
+$colInSecondRow.addClass( addColResAndSize('xs', 6) );
+$colInThirdRow.addClass( addColResAndSize('md', 3) );
+
+$colInFirstRow.eq(0).attr('id', 'aaa')
+$colInFirstRow.eq(1).attr('id', 'ccc');
+$colInFirstRow.eq(2).attr('id', 'ooo');
+
+$colInSecondRow.eq(0).css('color', 'red');
+$colInSecondRow.eq(1).css('color', 'red');
+
+$colInThirdRow.eq(0).attr('data-product', '300');
+$colInThirdRow.eq(1).attr('data-product', '400');
+$colInThirdRow.eq(2).attr('data-product', '500');
 
 
-$colInRow.eq(5).addClass( addColResAndSize('md', 6) ).attr('id', 'value');
-
-
+$paragraph.eq(0).text('Jeden');
+$paragraph.eq(1).text('Dwa');
+$paragraph.eq(2).text('Trzy');
+$paragraph.eq(3).text('Cztery');
+$paragraph.eq(4).text('Pięć');
+$paragraph.eq(5).text('Sześć');
+$paragraph.eq(6).text('Siedem');
+$paragraph.eq(7).text('Osiem');
 });
 
